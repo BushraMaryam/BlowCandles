52e2a191d8501e87fd32773148f8cf4ccdc9b0cb
BushraMaryam <bushramaryam411@gmail.com>
2025-02-24 13:18:00 +0500

    DepoymentConfigsAdded

--- app.py
+++ app.py
@@ -21,4 +21,5 @@ def handle_audio(data):
         socketio.emit('blow_out_candles')
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app, host="0.0.0.0", port=10000, debug=True)
+
